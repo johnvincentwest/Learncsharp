b624c0a5d39d4212fa37e7b71681b3c522889ef2
johnvincentwest <johnvincentwest@gmail.com>
2017-10-20 05:38:53 -0700

    First commit in learning csharp and github

---
+++ learningcsharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace learningcsharp
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.WriteLine("Hello this is my trying to learn C#");
+			Console.WriteLine("Hope this helps me document my learnings");
+			Console.WriteLine("I am also quite new to GitHub");
+
+			Console.WriteLine("What is your first name?");
+			string firstName = Console.ReadLine();
+			Console.WriteLine("First name: " + firstName);
+			Console.WriteLine("What is your last name?");
+			string lastName = Console.ReadLine();
+			Console.WriteLine("Last name: " + lastName);
+			Console.WriteLine("Your full name is " + firstName + " " + lastName);
+		}
+	}
+}
